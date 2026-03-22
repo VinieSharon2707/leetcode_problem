e590d4147cc0350114d7d7febd07f638f4e9733e
VinieSharon2707 <37viniesharon@gmail.com>
2026-03-22 21:02:56 +0530

    Time: 8 ms (92.11%) | Memory: 46.1 MB (40.08%) - LeetSync

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -1,6 +1,8 @@
+
 public class Solution {
     public boolean isPowerOfThree(int n) {
-        int maxPowerOf3 = 1162261467; // 3^19 is the largest power of 3 in int range
-        return n > 0 && maxPowerOf3 % n == 0;
+        if (n == 1) return true;
+        if (n <= 0 || n % 3 != 0) return false;
+        return isPowerOfThree(n / 3);
     }
 }
